86a06aa227d4272b3cee802ced097024a6e46edb
Nitay Neeman <nitay14@gmail.com>
2015-03-25 21:31:53 +0200

    Added a logic of context-menu disabling

--- src/ng-prevent.js
+++ src/ng-prevent.js
@@ -11,17 +11,35 @@ angular
                 console.log('Hello World!', $scope);
             },
             link: function (scope, element) {
+                var disableUserSelect = function () {
+                    element.css('user-select', 'none');
+                    element.css('-ms-user-select', 'none');
+                    element.css('-moz-user-select', 'none');
+                    element.css('-khtml-user-select', 'none');
+                    element.css('-webkit-user-select', 'none');
+                    element.css('-webkit-touch-callout', 'none');
+
+                };
+
+                var disableContextMenu = function () {
+                    element.bind('contextmenu', function (event) {
+                        event.preventDefault();
+                    });
+
+                };
+
                 if (angular.isDefined(scope.preventOptions)) {
                     if (scope.preventOptions.disableSelection) {
-                        element.css('user-select', 'none');
-                        element.css('-ms-user-select', 'none');
-                        element.css('-moz-user-select', 'none');
-                        element.css('-khtml-user-select', 'none');
-                        element.css('-webkit-user-select', 'none');
-                        element.css('-webkit-touch-callout', 'none');
+                        disableUserSelect();
+                    }
+
+                    if (scope.preventOptions.disableContextMenu) {
+                        disableContextMenu();
                     }
                 }
                 else {
+                    disableUserSelect();
+                    disableContextMenu();
                     $log.error('preventOptions is not defined!')
                 }
             }
