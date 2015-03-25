9d4fbda4a650ab97b0753287a3652d59463e7a47
Nitay Neeman <nitay14@gmail.com>
2015-03-25 21:14:51 +0200

    Added a bit of logic of selection disabling

--- src/ng-prevent.js
+++ src/ng-prevent.js
@@ -1,11 +1,29 @@
-angular.module('ngPrevent', []);
+angular
+    .module('ngPrevent', []);
 
-angular.module('ngPrevent')
-    .directive('prevent', function () {
+angular
+    .module('ngPrevent')
+    .directive('prevent', function ($log) {
         return {
             restrict: 'A',
+            scope: true,
             controller: function ($scope) {
-                console.log('Hello World!');
+                console.log('Hello World!', $scope);
+            },
+            link: function (scope, element) {
+                if (angular.isDefined(scope.preventOptions)) {
+                    if (scope.preventOptions.disableSelection) {
+                        element.css('user-select', 'none');
+                        element.css('-ms-user-select', 'none');
+                        element.css('-moz-user-select', 'none');
+                        element.css('-khtml-user-select', 'none');
+                        element.css('-webkit-user-select', 'none');
+                        element.css('-webkit-touch-callout', 'none');
+                    }
+                }
+                else {
+                    $log.error('preventOptions is not defined!')
+                }
             }
         };
     });
