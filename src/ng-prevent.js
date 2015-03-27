a9a0e2f204147abdb5651dc6705ba0f3a03c7837
Nitay Neeman <nitay14@gmail.com>
2015-03-27 11:16:53 +0300

    Moved the logic into Prevent service

--- src/ng-prevent.js
+++ src/ng-prevent.js
@@ -3,34 +3,31 @@ angular
 
 angular
     .module('ngPrevent')
-    .directive('prevent', function ($log, $parse) {
+    .service('Prevent', function () {
+        this.userSelect = function (element) {
+            element.css('user-select', 'none');
+            element.css('-ms-user-select', 'none');
+            element.css('-moz-user-select', 'none');
+            element.css('-khtml-user-select', 'none');
+            element.css('-webkit-user-select', 'none');
+            element.css('-webkit-touch-callout', 'none');
+        };
+
+        this.contextMenu = function (element) {
+            element.bind('contextmenu', function (event) {
+                event.preventDefault();
+            });
+        };
+    });
+
+angular
+    .module('ngPrevent')
+    .directive('prevent', function ($parse, Prevent) {
         return {
             restrict: 'A',
             scope: true,
             link: function (scope, element, attrs) {
                 var localPreventOptions;
-
-                var disableUserSelect = function () {
-                    element.css('user-select', 'none');
-                    element.css('-ms-user-select', 'none');
-                    element.css('-moz-user-select', 'none');
-                    element.css('-khtml-user-select', 'none');
-                    element.css('-webkit-user-select', 'none');
-                    element.css('-webkit-touch-callout', 'none');
-                };
-
-                var disableLeftClick = function () {
-                    element.bind('click', function (event) {
-                        event.preventDefault();
-                    });
-
-                };
-
-                var disableContextMenu = function () {
-                    element.bind('contextmenu', function (event) {
-                        event.preventDefault();
-                    });
-                };
 
                 if (attrs.prevent !== "") {
                     localPreventOptions = $parse(attrs.prevent)(scope);
@@ -40,19 +37,18 @@ angular
                 }
 
                 if (angular.isDefined(localPreventOptions)) {
-                    if (localPreventOptions.disableSelection) {
-                        disableUserSelect();
+                    if (localPreventOptions.disableUserSelect) {
+                        Prevent.userSelect(element);
                     }
 
                     if (localPreventOptions.disableContextMenu) {
-                        disableContextMenu();
+                        Prevent.contextMenu(element);
                     }
                 }
 
                 else {
-                    disableUserSelect();
-                    disableLeftClick();
-                    disableContextMenu();
+                    Prevent.userSelect(element);
+                    Prevent.contextMenu(element);
                 }
             }
         };
