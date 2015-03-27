486238e0aab1a6be33c7a15093f6d48448555c9c
Nitay Neeman <nitay14@gmail.com>
2015-03-27 18:57:51 +0300

    Added gulp-traceur

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var traceur = require('gulp-traceur');
 
-gulp.task('compress', function () {
+gulp.task('default', function () {
+    return gulp.src('src/*.js')
+        .pipe(traceur())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('dist', function () {
     gulp.src('src/*.js')
+        .pipe(traceur())
         .pipe(uglify({
             mangle: false
         }))
