03411fd2829848c2610787e6d4ad56a29fcf7ca8
Nitay Neeman <nitay14@gmail.com>
2015-03-28 15:41:26 +0300

    Added karma coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,26 @@
 module.exports = function (config) {
     config.set({
         basePath: '',
-        frameworks: ['jasmine'],
-        files: [],
-        exclude: [],
-        reporters: ['progress'],
+        frameworks: [
+            'jasmine'
+        ],
+        files: [
+            'bower_components/angular/angular.js',
+            'src/**/*.js'
+        ],
+        preprocessors: {
+            'src/**/*.js': ['coverage']
+        },
+        exclude: [
+
+        ],
+        reporters: [
+            'progress', 'coverage'
+        ],
         port: 9876,
         autoWatch: false,
-        browsers: ['PhantomJS']
+        browsers: [
+            'PhantomJS'
+        ]
     });
 };
