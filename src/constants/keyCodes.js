198f68abfd935cdca4a11c8304c6e067156a131a
Nitay Neeman <nitay14@gmail.com>
2015-04-03 10:41:33 +0300

    Created key codes constants map

---
+++ src/constants/keyCodes.js
@@ -0,0 +1,12 @@
+angular
+    .module('ngPrevent', []);
+
+angular
+    .module('ngPrevent')
+    .consatnt('keyCodes', {
+        'Backspace': 8,
+        'Tab': 9,
+        'Enter': 13,
+        'F11': 122,
+        'F12': 123
+    });
