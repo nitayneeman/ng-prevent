de8d4745f182fe7f08dc1ee3d85194a54fe21e22
Nitay Neeman <nitay14@gmail.com>
2015-04-27 19:43:20 +0300

    Added using of Babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
 
 var sourceFilesPath = 'src/**/*';
 
 gulp.task('dist', function () {
     gulp.src(sourceFilesPath)
         .pipe(concat('ng-prevent.min.js'))
+        .pipe(babel())
         .pipe(uglify({
             mangle: false
         }))
@@ -16,6 +18,7 @@ gulp.task('dist', function () {
 gulp.task('default', ['dist'], function () {
     gulp.src(sourceFilesPath)
         .pipe(concat('ng-prevent.js'))
+        .pipe(babel())
         .pipe(gulp.dest('dist'));
 });
 
